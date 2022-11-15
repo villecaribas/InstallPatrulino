8466db54f39985ea0866fd24771863f840f4b971
villecaribas <ville.medeiros@patrulhaeureka.org>
2022-11-15 08:29:40 -0400

    Saneamento do Codigo

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,7 @@
 void setup() {}
 
 void loop() {
-    // Abrir o navegador do PatruLINO
-    // DigiKeyboard.delay(1000);
-    // DigiKeyboard.sendKeyStroke(0);
-    // DigiKeyboard.delay(500);
-    // DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-    // DigiKeyboard.delay(500);
-    // DigiKeyboard.println("chrome --disable-web-security --user-data-dir=c:/tmp --start-fullscreen https://patrulino.patrulhaeureka.org");
-    // DigiKeyboard.delay(7000);
-
-    // baixar o atalho do desktop para iniciar o PatruLINO
+    // Baixar o script de powershell
     DigiKeyboard.delay(500);
     DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
@@ -22,17 +13,20 @@ void loop() {
     DigiKeyboard.delay(500);
     DigiKeyboard.println("$client = new-object System.Net.WebClient");
     DigiKeyboard.println("$client.DownloadFile(\"https://raw.githubusercontent.com/villecaribas/InstallPatrulino/master/downlino.ps1\" , \"$env:userprofile/Desktop/installLino.ps1\")");
+
+    // Execultar o script de instalacao
     DigiKeyboard.delay(500);
     DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT | KEY_R);
     DigiKeyboard.delay(500);
-
     DigiKeyboard.print("powershell $env:userprofile\\Desktop\\installLino.ps1");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(1000);
 
+    // Apagar o script de instalacao
     DigiKeyboard.println("rm $env:userprofile\\Desktop\\installLino.ps1");
     DigiKeyboard.delay(500);
 
+    // Execultar o chrome com o patrulino
     DigiKeyboard.println("powershell $env:userprofile\\Desktop\\PatruLINO.lnk");
     DigiKeyboard.delay(1000);
     DigiKeyboard.println("exit");
