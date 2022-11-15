5018e528e447ca9307359a42eb8364dff1ef123d
villecaribas <ville.medeiros@patrulhaeureka.org>
2022-11-15 07:52:22 -0400

    criando script

---
+++ downlino.ps1
@@ -0,0 +1,10 @@
+mkdir c:\\tmp
+
+$client = new-object System.Net.WebClient
+$client.DownloadFile("https://github.com/villecaribas/patrulino/raw/main/img/lino.ico","c:\\tmp\lino.ico")
+$client.DownloadFile("https://github.com/villecaribas/patrulino/raw/main/PatruLINO.lnk" , "$env:userprofile/Desktop/PatruLINO.lnk")
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+$shortcut = $WScriptShell.CreateShortcut("$env:userprofile\Desktop\PatruLINO.lnk")
+$shortcut.IconLocation = "C:\\tmp\lino.ico"
+$shortcut.Save()
--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,29 @@ void setup() {}
 
 void loop() {
     // Abrir o navegador do PatruLINO
+    // DigiKeyboard.delay(1000);
     // DigiKeyboard.sendKeyStroke(0);
     // DigiKeyboard.delay(500);
     // DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     // DigiKeyboard.delay(500);
-    // DigiKeyboard.println("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe --disable-web-security --user-data-dir=c:\\tmp --start-fullscreen https://patrulino.patrulhaeureka.org");
-    // delay(5000);
+    // DigiKeyboard.println("chrome --disable-web-security --user-data-dir=c:/tmp --start-fullscreen https://patrulino.patrulhaeureka.org");
+    // DigiKeyboard.delay(7000);
 
     // baixar o atalho do desktop para iniciar o PatruLINO
-    DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.delay(500);
+    DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     DigiKeyboard.delay(500);
     DigiKeyboard.println("powershell");
     DigiKeyboard.delay(500);
     DigiKeyboard.println("$client = new-object System.Net.WebClient");
-    DigiKeyboard.delay(500);
-    DigiKeyboard.println("$client.DownloadFile(\"https://github.com/villecaribas/patrulino/raw/main/PatruLINO.lnk\" , \"$env:userprofile/Desktop/teste1.lnk\")");
-    DigiKeyboard.delay(2000);
+    
+    DigiKeyboard.println("$client.DownloadFile(\"https://github.com/villecaribas/patrulino/raw/main/PatruLINO.lnk\" , \"$env:userprofile/Desktop/PatruLINO.lnk\")");
+    delay(1000);
     DigiKeyboard.println("$client.DownloadFile(\"https://github.com/villecaribas/patrulino/raw/main/img/lino.ico\" , \"c:\\tmp\\lino.ico\")");
+    delay(1000);
+   
+    DigiKeyboard.delay(7000);
 
     // DigiKeyboard.println("exit");
 
